835db0f894ec8fe0ad532fcbb0330ade81dc4cb6
Ling <wssgcg1213@qq.com>
2015-12-10 00:37:58 +0800

    完善抱紧

--- src/common/bootstrap/crontab.js
+++ src/common/bootstrap/crontab.js
@@ -4,6 +4,7 @@
  * @Email i@zeroling.com
  */
 import nodemailer from 'nodemailer';
+import fs from 'fs';
 const config = think.config();
 let mail = nodemailer.createTransport({
     host: config.mailHost,
@@ -13,16 +14,48 @@ let mail = nodemailer.createTransport({
         pass: config.mailPassword
     }
 });
-let modelCheck = think.model('check', config.db);
+function fsLogStat (file, infoObj) {
+    if (infoObj === undefined) {
+        return new Promise((resolve, reject) => {
+            fs.readFile(__dirname + file, (err, content) => {
+                if (err) {
+                    resolve(false);
+                } else {
+                    resolve(content);
+                }
+            });
+        });
+    }
+    let str = JSON.stringify(infoObj);
+    return new Promise((resolve, reject) => {
+        fs.appendFile(__dirname + file, str, (err, status) => {
+            if (err) {
+                resolve(false);
+            } else {
+                resolve(status);
+            }
+        })
+    });
+}
+let modelCheck = think.model('cet', config.db);
 setInterval(() => {
-    modelCheck.add({ts: +new Date()}).catch(e => {
-        let mailContent = {
-            from: 'CETB报警 ' + config.mailAccount, // sender address
-            to: 'i@zeroling.com', // list of receivers
-            subject: 'CETB报警,Mysql挂掉啦! o(>﹏<)o', // Subject line
-            html: '<b>Mysql挂掉了</b>' // html body
-        };
-        mail.sendMail(mailContent, cbHandler);
+    modelCheck.count().catch(e => {
+        fsLogStat('stat').then(stat => {
+            if (stat && (+new Date) - stat.lastAlertTs < 120 * 1000) {
+                //do nothing
+            } else {
+                let mailContent = {
+                    from: 'CETB报警 ' + config.mailAccount, // sender address
+                    to: 'i@zeroling.com', // list of receivers
+                    subject: 'CETB报警,Mysql挂掉啦! o(>﹏<)o' + ((stat || '') && (stat.info || '')), // Subject line
+                    html: '<b>Mysql挂掉了</b>' // html body
+                };
+                mail.sendMail(mailContent, cbHandler);
+                fsLogStat('stat', {
+                    lastAlertTs: +new Date
+                });
+            }
+        });
     });
 }, 120 * 1000);
 
